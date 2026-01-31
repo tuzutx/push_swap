f06f707e34305928e1e20ab0892dc13b3ff5b821
tuzutx <naroaolaetxea@gmail.com>
2026-01-31 23:03:59 +0100

    Sort3 hecho

--- src/algorithm_sort3.c
+++ src/algorithm_sort3.c
@@ -12,7 +12,44 @@
 
 #include "../include/push_swap.h"
 
-void	sort_three()
+int	higher(t_list *a)
 {
-	
+	int	higher;
+	int	indx;
+
+	set_index(a);
+	higher = a->content;
+	indx = a->index;
+	a = a->next;
+	while (a)
+	{
+		if (higher < a->content)
+		{
+			indx = a->index;
+			higher = a->content;
+		}
+		a = a->next;
+	}
+}
+
+void	sort_three(t_push_swap *data, t_list *a)
+{
+	int	higher;
+
+	higher = higher(a);
+	if (higher == 1)
+	{
+		rotate(data->a);
+		ft_printf("ra\n");
+	}
+	else if (higher == 2)
+	{
+		reverser_rotate(data->a);
+		ft_printf("ra\n");
+	}
+	if (a->content > (a->next)->content)
+	{
+		swap(data->a);
+		ft_printf("sa\n");
+	}
 }
